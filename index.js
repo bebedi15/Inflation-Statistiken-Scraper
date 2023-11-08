e2b22d36cd0cb5c5a76196697156e20bdcfff460
bebedi15 <70455072+bebedi15@users.noreply.github.com>
2023-11-08 15:08:34 +0100

    Update index.js
    [FIXED]
    
    + und - Werte werden jetzt angezeigt

--- index.js
+++ index.js
@@ -1,116 +1,231 @@
 const axios = require('axios');
+
 const cheerio = require('cheerio');
+
 const cron = require('node-cron');
+
 const fs = require('fs');
+
 const path = require('path');
+
 const FormData = require('form-data');
 
+
+
 const DISCORD_WEBHOOK_URL = '';
+
 const DATA_FILE_PATH = 'data.json';
+
 const IMAGE_URL = 'https://www.mehrwertsteuerrechner.de/wp-content/uploads/inflation/Inflation-Deutschland.png';
+
 const IMAGE_PATH = 'image.png';
 
+
+
 async function scrapeWebsite() {
+
   try {
+
     const url = 'https://www.destatis.de/DE/Themen/Wirtschaft/Preise/Verbraucherpreisindex/_inhalt.html';
+
     const response = await axios.get(url);
+
     const $ = cheerio.load(response.data);
 
+
+
     const inflationData = {
+
       Inflationsrate: '',
+
       VerbraucherpreiseEnergie: '',
+
       VerbraucherpreiseNahrungsmittel: '',
+
     };
+
+
 
     const targetElements = $('.c-stat__link');
 
+
+
     targetElements.each((index, element) => {
+
       const text = $(element).text().trim();
+
       if (text.includes('Inflations­rate')) {
-        const matches = text.match(/\+\s*(\d+,\d+)\s*%/);
+
+        const matches = text.match(/([-+?])\s*(\d+,\d+)\s*%/);
+
         if (matches) {
-          inflationData.Inflationsrate = `+${matches[1]}`;
+
+          inflationData.Inflationsrate = `${matches[1]}${matches[2]}`;
+
         }
+
       } else if (text.includes('Verbraucherpreise Energie')) {
-        const matches = text.match(/(\d+,\d+)\s*%/);
+
+        const matches = text.match(/([-+]?)\s*(\d+,\d+)\s*%/);
+
         if (matches) {
-          inflationData.VerbraucherpreiseEnergie = `${matches[1]}`;
+
+          inflationData.VerbraucherpreiseEnergie = `${matches[1]}${matches[2]}`;
+
         }
+
       } else if (text.includes('Verbraucherpreise Nahrungsmittel')) {
-        const matches = text.match(/(\d+,\d+)\s*%/);
+
+        const matches = text.match(/([-+]?)\s*(\d+,\d+)\s*%/);
+
         if (matches) {
-          inflationData.VerbraucherpreiseNahrungsmittel = `${matches[1]}`;
+
+          inflationData.VerbraucherpreiseNahrungsmittel = `${matches[1]}${matches[2]}`;
+
         }
+
       }
+
     });
+
+
 
     // Save the extracted data to the data.json file
 
+
+
     // Check for changes and send a message to Discord
+
     const oldData = JSON.parse(fs.readFileSync(DATA_FILE_PATH, 'utf8'));
 
+
+
     const hasChanged =
+
       Object.keys(oldData).length === 0 || JSON.stringify(oldData) !== JSON.stringify(inflationData);
 
+
+
     if (hasChanged) {
+
       fs.writeFileSync(DATA_FILE_PATH, JSON.stringify(inflationData), 'utf8');
+
       const imageResponse = await axios.get(IMAGE_URL, { responseType: 'stream' });
+
       const imagePath = path.join(__dirname, IMAGE_PATH);
 
+
+
       const imageFile = fs.createWriteStream(imagePath);
+
       imageResponse.data.pipe(imageFile);
 
+
+
       await new Promise((resolve, reject) => {
+
         imageFile.on('finish', resolve);
+
         imageFile.on('error', reject);
+
       });
 
+
+
       const message = {
+
         content: 'Es gab eine Änderung in den Daten:',
+
         embeds: [
+
           {
+
             title: 'Neue Daten',          
+
             description: `*Veränderungsraten zum Vorjahresmonat in %* \n\r**Inflationsrate (vorläufig):** \`${inflationData.Inflationsrate || 'Nicht gefunden'} %\`\n**Verbraucherpreise Energie:** \`${inflationData.VerbraucherpreiseEnergie || 'Nicht gefunden'} %\`\n**Verbraucherpreise Nahrungsmittel:** \`${inflationData.VerbraucherpreiseNahrungsmittel || 'Nicht gefunden'} %\` \n\r [Quelle](https://www.destatis.de/DE/Themen/Wirtschaft/Preise/Verbraucherpreisindex/_inhalt.html)`,
+
             color: 0x00ff00,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+
             image: {
+
               url: `attachment://${IMAGE_PATH}`,
+
             },
+
           },
+
         ],
+
       };
+
+
 
       const formData = new FormData();
+
       formData.append('payload_json', JSON.stringify(message));
+
       formData.append('file', fs.createReadStream(imagePath));
 
+
+
       const requestOptions = {
+
         method: 'POST',
+
         headers: {
+
           ...formData.getHeaders(),
+
         },
+
         data: formData,
+
         url: DISCORD_WEBHOOK_URL,
+
       };
+
+
 
       const response = await axios(requestOptions);
 
+
+
       console.log('Nachricht an Discord-Webhook gesendet.');
+
     } else {
+
       console.log('Keine Änderung in den Daten festgestellt.');
+
     }
+
   } catch (error) {
+
     console.error('Fehler beim Scrapen der Website:', error);
+
   }
+
 }
+
+
 
 function startScraping() {
+
   console.log('Programm wird gestartet...');
+
   scrapeWebsite();
 
+
+
   cron.schedule('*/30 * * * *', () => {
+
     console.log('Scraper wird alle 30 Minuten ausgeführt...');
+
     scrapeWebsite();
+
   });
+
 }
+
+
 
 startScraping();
