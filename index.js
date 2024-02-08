1ced15d38aaf1b57dbcea42a580c476986198d9a
bebedi15 <70455072+bebedi15@users.noreply.github.com>
2024-02-08 23:12:01 +0100

    Version 2.0

--- index.js
+++ index.js
@@ -12,17 +12,18 @@ const FormData = require('form-data');
 
 const {AttachmentBuilder} = require('discord.js')
 
+const config = require('./config.json')
 
 
-const DISCORD_WEBHOOK_URL = 'WEBHOOK_URL_HERE';
 
-const DATA_FILE_PATH = 'data.json';
 
+const inflationdata = 'inflation_data.json';
+const bruttoinlandsproduktdata = "bip_data.json"
 const IMAGE_URL = 'https://www.mehrwertsteuerrechner.de/wp-content/uploads/inflation/Inflation-Deutschland.png';
 
 
 
-async function scrapeWebsite() {
+async function scrapeinflation() {
 
   try {
 
@@ -96,7 +97,7 @@ async function scrapeWebsite() {
 
     // Check for changes and send a message to Discord
 
-    const oldData = JSON.parse(fs.readFileSync(DATA_FILE_PATH, 'utf8'));
+    const oldData = JSON.parse(fs.readFileSync(inflationdata, 'utf8'));
 
 
 
@@ -108,7 +109,7 @@ async function scrapeWebsite() {
 
     if (hasChanged) {
 
-      fs.writeFileSync(DATA_FILE_PATH, JSON.stringify(inflationData), 'utf8');
+      fs.writeFileSync(inflationdata, JSON.stringify(inflationData), 'utf8');
 
 
       const file = new AttachmentBuilder(IMAGE_URL)
@@ -162,7 +163,7 @@ async function scrapeWebsite() {
 
         data: formData,
 
-        url: DISCORD_WEBHOOK_URL,
+        url: config.webhooks['inflation-webhook'],
 
       };
 
@@ -190,20 +191,178 @@ async function scrapeWebsite() {
 
 
 
+async function scrapebip() {
+
+  try {
+
+    const url = 'https://www.destatis.de/DE/Themen/Wirtschaft/Volkswirtschaftliche-Gesamtrechnungen-Inlandsprodukt/_inhalt.html';
+
+    const response = await axios.get(url);
+
+    const $ = cheerio.load(response.data);
+
+
+
+    const bipdata = {
+
+      wirtschaftswachtum: '',
+
+      jeweiligepreisen: ''
+
+    };
+
+
+
+    const targetElements = $('.c-stat__link');
+
+
+
+    targetElements.each((index, element) => {
+
+      const text = $(element).text().trim();
+
+      if (text.includes('Wirtschafts-')) {
+
+        const matches = text.match(/([-+?])\s*(\d+,\d+)\s*%/);
+
+        if (matches) {
+
+          bipdata.wirtschaftswachtum = `${matches[1]}${matches[2]}`;
+
+        }
+
+      } else if (text.includes('in jeweiligen Preisen')) {
+
+        const matches = text.match(/(\d+(\s\d{3})*\sMrd\.?\s€)/); // Das angepasste reguläre Ausdrucksmuster
+
+        if (matches) {
+          const cleanText = matches[0].replace(/\s|\n/g, ''); // Entferne alle Leerzeichen und Zeilenumbrüche
+          bipdata.jeweiligepreisen = cleanText;
+        }
+
+      }
+
+    });
+
+
+
+    // Save the extracted data to the data.json file
+
+
+
+    // Check for changes and send a message to Discord
+
+    const oldData = JSON.parse(fs.readFileSync(bruttoinlandsproduktdata, 'utf8'));
+
+
+
+    const hasChanged =
+
+      Object.keys(oldData).length === 0 || JSON.stringify(oldData) !== JSON.stringify(bipdata);
+
+
+
+    if (hasChanged) {
+
+      fs.writeFileSync(bruttoinlandsproduktdata, JSON.stringify(bipdata), 'utf8');
+
+
+
+
+
+      const message = {
+
+        content: 'Es gab eine Änderung in den Daten:',
+
+        embeds: [
+
+          {
+
+            title: 'Neue Daten',          
+
+            description: `*Veränderungsraten* \n\r**Wirtschafts-wachstum:** \`${bipdata.wirtschaftswachtum || 'Nicht gefunden'} %\`\n**In jeweiligen Preisen:** \`${bipdata.jeweiligepreisen || 'Nicht gefunden'}\`\n\r [Quelle](https://www.destatis.de/DE/Themen/Wirtschaft/Volkswirtschaftliche-Gesamtrechnungen-Inlandsprodukt/_inhalt.html)`,
+
+            color: 0x00ff00,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+
+          },
+
+        ],
+
+      };
+
+
+
+      const formData = new FormData();
+
+      formData.append('payload_json', JSON.stringify(message));
+
+
+
+
+      const requestOptions = {
+
+        method: 'POST',
+
+        headers: {
+
+          ...formData.getHeaders(),
+
+        },
+
+        data: formData,
+
+        url: config.webhooks['Bruttoinlandsprodukt-webhook'],
+
+      };
+
+
+
+      const response = await axios(requestOptions);
+
+
+
+      console.log('Nachricht an Discord-Webhook gesendet.');
+
+    } else {
+
+      console.log('Keine Änderung in den Daten festgestellt.');
+
+    }
+
+  } catch (error) {
+
+    console.error('Fehler beim Scrapen der Website:', error);
+
+  }
+
+}
+
+
+
+
+
 function startScraping() {
 
   console.log('Programm wird gestartet...');
 
-  scrapeWebsite();
-
+  if (config.toggle.inflation == true && config.webhooks['inflation-webhook'] != "") {
+    scrapeinflation();
+  }
+  if (config.toggle.Bruttoinlandsprodukt == true && config.webhooks['Bruttoinlandsprodukt-webhook'] != "") {
+    scrapebip();
+  }
 
 
   cron.schedule('*/30 * * * *', () => {
 
     console.log('Scraper wird alle 30 Minuten ausgeführt...');
 
-    scrapeWebsite();
-
+    if (config.toggle.inflation == true && config.webhooks['inflation-webhook'] != "") {
+      scrapeinflation();
+    }
+    if (config.toggle.Bruttoinlandsprodukt == true && config.webhooks['Bruttoinlandsprodukt-webhook'] != "") {
+      scrapebip();
+    }
   });
 
 }
